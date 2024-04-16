72441e385ba61bf4891e03d0798b8809db586ebe
Vaibhav Rastogi <rastogi04vaibhav@gmail.com>
2024-04-16 15:45:15 +0530

    done with register

--- src/components/register/Register.tsx
+++ src/components/register/Register.tsx
@@ -5,61 +5,76 @@ export default function Register() {
   const navigate = useNavigate();
   return (
     <section className={registerStyles.registerSection}>
-      <h2 className={registerStyles.register}>Register</h2>
-      <button type="button" className={registerStyles.registerWithGoogleBtn}>
-        Sign up with Google
-      </button>
-      <hr
-        className={registerStyles.registerDivider}
-        data-content="or register with email"
-      />
-      <form action="" className={registerStyles.form}>
-        <div>
-          <label htmlFor="name" className={registerStyles.nameLabel}>
-            Name
-          </label>
-          <label htmlFor="username" className={registerStyles.userNameLabel}>
-            Username
-          </label>
-          <input
-            type="text"
-            style={{ width: "30%" }}
-            name="name"
-            className={registerStyles.nameInputBox}
-          />
-          <input
-            type="text"
-            style={{ width: "30%" }}
-            name="username"
-            className={registerStyles.userNameInputBox}
-          />
-        </div>
-        <label className={registerStyles.label}>Email</label>
-        <input type="email" className={registerStyles.registerPageInputBox} />
-        <label className={registerStyles.label}>Password</label>
-        <input
-          type="password"
-          name=""
-          className={registerStyles.registerPageInputBox}
-        />
-        <label className={registerStyles.label}>
-          <input type="checkbox" name="" id="" />
-          Remember Me
-        </label>
-        <a href="" className={registerStyles.a}>
-          Forgot?
-        </a>
-        <button
-          className={registerStyles.registerButton}
-          onClick={() => navigate("/player")}
-        >
-          Register
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <h2 className={registerStyles.register}>Register</h2>
+        <button type="button" className={registerStyles.registerWithGoogleBtn}>
+          Sign up with Google
         </button>
-      </form>
-      <p id={registerStyles.signInRedirectButton}>Already have an account?</p>
-      <a href="" id={registerStyles.signInRedirectLink}>
-        Login
-      </a>
+        <hr
+          className={registerStyles.registerDivider}
+          data-content="or register with email"
+        />
+        <form action="" className={registerStyles.form}>
+          <div className={registerStyles.nameAndUserNameContainer}>
+            <fieldset className={registerStyles.fieldset}>
+              <label htmlFor="name" className={registerStyles.nameLabel}>
+                Name
+              </label>
+              <input
+                type="text"
+                name="name"
+                className={registerStyles.nameInputBox}
+              />
+            </fieldset>
+            <fieldset className={registerStyles.fieldset}>
+              <label
+                htmlFor="username"
+                className={registerStyles.userNameLabel}
+              >
+                Username
+              </label>
+              <input
+                type="text"
+                name="username"
+                className={registerStyles.userNameInputBox}
+              />
+            </fieldset>
+          </div>
+          <label className={registerStyles.label}>Email</label>
+          <input type="email" className={registerStyles.registerPageInputBox} />
+          <label className={registerStyles.label}>Password</label>
+          <input
+            type="password"
+            name=""
+            className={registerStyles.registerPageInputBox}
+          />
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <label className={registerStyles.label}>
+              <input type="checkbox" name="" id="" />
+              Remember Me
+            </label>
+            <a href="" className={registerStyles.a}>
+              Forgot?
+            </a>
+          </div>
+          <button
+            className={registerStyles.registerButton}
+            onClick={() => navigate("/player")}
+          >
+            Register
+          </button>
+        </form>
+        <p id={registerStyles.signInRedirectButton}>Already have an account?</p>
+        <a href="" id={registerStyles.signInRedirectLink}>
+          Login
+        </a>
+      </div>
     </section>
   );
 }
--- src/routes/loginPage/LoginPage.tsx
+++ src/routes/loginPage/LoginPage.tsx
@@ -20,6 +20,7 @@ export default function LoginPage(): JSX.Element {
 
       <section className={loginPageStyles.loginPageContent}>
         <Register />
+        {/* <Login /> */}
       </section>
     </main>
   );
